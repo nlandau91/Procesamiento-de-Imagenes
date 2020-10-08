0e2c84e8fb77bbf416c38e05ca2c7567eb111045
Nicolas Landau <nlandau91@gmail.com>
2020-10-08 18:27:37 -0300

    Terminado, faltan finos

--- Actividad3/Actividad3.cpp
+++ Actividad3/Actividad3.cpp
@@ -4,19 +4,64 @@
 using namespace std;
 using namespace cv;
 
-int main()
+int main(int argc, char* argv[])
 {
+    string src_img = "car.jpg";
+	// Check the number of parameters
+	if (argc < 2) {
+		std::cout << "No se ingreso un parametro, usando nombre de imagen por defecto 'car.jpg'" << endl;
+	}
+	else
+	{
+		src_img = argv[1];
+	}
     //cargamos la imagen
-    Mat src = imread("buzz.jpg", IMREAD_COLOR);
+    Mat src = imread(src_img, IMREAD_COLOR);
     imshow("orig",src);
     waitKey(200);
-    //(1) aplicamos filtro de la mediana
     
-    //(2) detectamos bordes
+    //(1) aplicamos filtro de la mediana
+    Mat afterMedianFilter;
+    int kSize = 5;
+    medianBlur(src,afterMedianFilter,kSize);
+    imshow("afterMedianFilter",afterMedianFilter);
+    waitKey(200);
+    
+    //(2) detectamos bordes de la imagen en escala de grises usando Laplacian
+    Mat grayscale;
+    cvtColor(src,grayscale,COLOR_BGR2GRAY);
+    Mat afterLaplace;
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
+    Laplacian(grayscale,afterLaplace,ddepth,kSize,scale,delta,BORDER_DEFAULT);
+    Mat absAfterLaplace;
+    convertScaleAbs(afterLaplace,absAfterLaplace);
+    Mat edges;
+    bitwise_not(absAfterLaplace, edges);
+    imshow("edges",edges);
+    waitKey(200);
     
     //(3) reducimos la cantidad de colores en (1)
+    Mat requant = afterMedianFilter.clone();
+    int quantScale = 24;
+    for(int i = 0; i < requant.rows; i++)
+    {
+        for(int j = 0; j < requant.cols; j++)
+        {
+           requant.at<Vec3b>(i,j)[0] = floor(requant.at<Vec3b>(i,j)[0] / quantScale) * quantScale;
+           requant.at<Vec3b>(i,j)[1] = floor(requant.at<Vec3b>(i,j)[1] / quantScale) * quantScale;
+           requant.at<Vec3b>(i,j)[2] = floor(requant.at<Vec3b>(i,j)[2] / quantScale) * quantScale;
+        }
+    }
+    imshow("requant",requant);
+    waitKey(200);
     
     //(4) agregamos a (3) los bordes obtenidos en (2)
+    Mat result;
+    bitwise_and(requant,requant,result,edges);
+    imshow("result",result);
+    waitKey(200);
     
     //done
     cout << "Presione enter para continuar" << endl;
